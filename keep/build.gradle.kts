28a50d44633219e4a6f5817714c3ad273b9cd16d
DESKTOP-0CH3G3T\Administrator <776241154@qq.com>
2022-06-02 13:31:49 +0800

    CREATE project

---
+++ keep/build.gradle.kts
@@ -0,0 +1,54 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.10"
+    `maven-publish`
+}
+
+group = "kotlinerx"
+version = "0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
+    val logbackVersion: String by project
+    implementation("ch.qos.logback:logback-classic:${logbackVersion}")
+    val kotlinLoggingVersion: String by project
+    implementation("io.github.microutils:kotlin-logging:${kotlinLoggingVersion}")
+    val sqliteVersion: String by project
+    // test
+    testImplementation("org.xerial:sqlite-jdbc:${sqliteVersion}")
+    testImplementation(kotlin("test"))
+}
+
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+publishing {
+    repositories {
+        maven {
+
+            name = "kotlinerx-keep"
+            url = uri("https://maven.pkg.github.com/kotlinerx/keep")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
